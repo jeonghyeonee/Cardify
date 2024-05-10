9ff697b42ec0ca8a392f3772e6a7276447997c53
jeonghyeonee <jlh1218@naver.com>
2024-05-11 01:28:58 +0900

    connect html and python

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
+# 전역 변수를 초기화합니다.
+card_info = {}
 
 @app.route('/')
 def index():
@@ -8,21 +11,22 @@ def index():
 
 @app.route('/validate', methods=['POST'])
 def validate():
-    card_number_1 = request.form['card_number_1']
-    card_number_2 = request.form['card_number_2']
-    card_number_3 = request.form['card_number_3']
-    card_number_4 = request.form['card_number_4']
-    
-    expiry_month = request.form['expiry_month']
-    expiry_year = request.form['expiry_year']
-    
-    # 각 카드 번호와 유효기간을 출력하거나 다른 작업을 수행할 수 있음
-    print(f'카드 번호: {card_number_1}-{card_number_2}-{card_number_3}-{card_number_4}')
-    print(f'유효기간: {expiry_month}/{expiry_year}')
-    
-    # 여기서 유효성 검사 로직을 추가할 수 있음
-    
+    global card_info
+    card_info = {
+        "card_number_1": request.form['card_number_1'],
+        "card_number_2": request.form['card_number_2'],
+        "card_number_3": request.form['card_number_3'],
+        "card_number_4": request.form['card_number_4'],
+        "expiry_month": request.form['expiry_month'],
+        "expiry_year": request.form['expiry_year']
+    }
+    print("카드 정보를 성공적으로 받았습니다.")
     return '카드 정보를 성공적으로 받았습니다.'
+
+@app.route('/card-info', methods=['GET'])
+def get_card_info():
+    global card_info
+    return jsonify(card_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- card_validity.py
+++ card_validity.py
@@ -1,1 +1,14 @@
 from heaan_utils import Heaan
+import requests
+
+# Flask 애플리케이션의 URL
+api_url = 'http://127.0.0.1:5000'
+
+# /card-info 엔드포인트에 GET 요청을 보내어 카드 정보를 가져옴
+response = requests.get(f'{api_url}/card-info')
+
+if response.status_code == 200:
+    card_info = response.json()
+    print("카드 정보:", card_info)
+else:
+    print('카드 정보를 가져오는 데 실패했습니다:', response.status_code)
