9fc56756dea2c4bfa9b90df39470839600297125
jeonghyeonee <jlh1218@naver.com>
2024-05-27 18:03:14 +0900

    update performance file

--- check_time.py
+++ check_time.py
@@ -1,6 +1,7 @@
 from heaan_utils import Heaan
 import time
 from card_validity import check_card_brand_method1, check_card_brand_method2, check_card_brand_method3
+
 he = Heaan()
 
 def measure_time(func, *args):
@@ -12,10 +13,23 @@ def measure_time(func, *args):
 def test_efficiency():
     times = {}
     for brand, ctxt in encrypted_test_cards.items():
-        _, time1 = measure_time(check_card_brand_method1, ctxt)
-        _, time2 = measure_time(check_card_brand_method2, ctxt)
-        _, time3 = measure_time(check_card_brand_method3, ctxt)
-        times[brand] = (time1, time2, time3)
+        method1_times = []
+        method2_times = []
+        method3_times = []
+        cnt = 0
+        for _ in range(100):
+            cnt += 1
+            print(brand+" cnt", cnt)
+            _, time1 = measure_time(check_card_brand_method1, ctxt)
+            _, time2 = measure_time(check_card_brand_method2, ctxt)
+            _, time3 = measure_time(check_card_brand_method3, ctxt)
+            method1_times.append(time1)
+            method2_times.append(time2)
+            method3_times.append(time3)
+        avg_time1 = sum(method1_times) / len(method1_times)
+        avg_time2 = sum(method2_times) / len(method2_times)
+        avg_time3 = sum(method3_times) / len(method3_times)
+        times[brand] = (avg_time1, avg_time2, avg_time3)
     return times
 
 # Sample card numbers for testing
@@ -31,7 +45,6 @@ test_card_numbers = {
 # Encrypt test card numbers
 encrypted_test_cards = {k: he.encrypt(he.feat_msg_generate(v)) for k, v in test_card_numbers.items()}
 
-
 efficiency_results = test_efficiency()
 for card, times in efficiency_results.items():
-    print(f"Card: {card}, \nMethod1 Time: {times[0]}, \nMethod2 Time: {times[1]}, \nMethod3 Time: {times[2]}")
+    print(f"Card: {card}, \nMethod1 Average Time: {times[0]}, \nMethod2 Average Time: {times[1]}, \nMethod3 Average Time: {times[2]}")
